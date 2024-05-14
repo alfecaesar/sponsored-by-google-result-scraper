80dbb879f0231aeebe61e967c442f8b29de13193
alfecaesar <alfecaesar@gmail.com>
2024-05-15 06:23:15 +0800

    minor changes

--- google_scraper.py
+++ google_scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import re
 
 # Set up Chrome WebDriver
 driver = webdriver.Chrome()
@@ -18,7 +19,7 @@ search_box.send_keys(Keys.RETURN)
 # Wait for the search results to load
 time.sleep(5)  # Adjust the time as needed
 
-# Capture screenshot of the newsfeed
+# Capture screenshot of the Sponsored list
 driver.find_element(By.CLASS_NAME, "pla-exp-container").screenshot("google-search-result-sponsored.png")
 
 # Get the information of all products under "Sponsored" list
@@ -27,7 +28,7 @@ for product in sponsored_products:
     product_title = product.find_element(By.CLASS_NAME, "pla-unit-title-link").text
     product_url = product.find_element(By.CLASS_NAME, "pla-unit-title-link").get_attribute("href")
     product_price = product.find_element(By.CLASS_NAME, "e10twf").text
-    product_rating_star = product.find_element(By.CLASS_NAME, "z3HNkc").get_attribute("aria-label")
+    product_rating_star = product.find_element(By.CLASS_NAME, "z3HNkc").get_attribute("aria-label").replace(',', '')
     product_rating_num_count = product.find_element(By.CLASS_NAME, "pbAs0b").text
     if(product_title):
         print("Title:", product_title)
@@ -36,9 +37,9 @@ for product in sponsored_products:
     if(product_price):
         print("Price:", product_price)
     if(product_rating_star):
-        print("Rating Start:", product_rating_star)
+        print("Rating Star:", product_rating_star)
     if(product_rating_num_count):
-        print("Rating Num Count:", product_rating_num_count)
+        print("Rating Num Count:", re.sub('[()]', '', product_rating_num_count))
     print("")
 
 # Close the browser
