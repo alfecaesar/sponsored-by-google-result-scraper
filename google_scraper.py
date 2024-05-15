e13c8f0c581700fd2911cc3a3f1efe70635d09b7
alfecaesar <alfecaesar@gmail.com>
2024-05-15 10:39:07 +0800

    changes on element validation

--- google_scraper.py
+++ google_scraper.py
@@ -18,45 +18,54 @@ search_box.send_keys(keyword)
 search_box.send_keys(Keys.RETURN)
 
 # Wait for the search results to load
-time.sleep(8)  # Adjust the time as needed
+time.sleep(5)  # Adjust the time as needed
 
 # Capture screenshot of the Sponsored list
-driver.find_element(By.CLASS_NAME, "pla-exp-container").screenshot("google-search-result-sponsored.png")
+try:
+    driver.find_element(By.CLASS_NAME, "pla-exp-container").screenshot("google-search-result-sponsored.png")
+    print("Sponsored list is found. Saving screenshot as google-search-result-sponsored.png")
+except NoSuchElementException:
+    print("Sponsored list is not found")
+
+print("*** Start of sponsored product list ***")
 
 # Get the information of all products under "Sponsored" list
 sponsored_products = driver.find_elements(By.CLASS_NAME, "pla-unit")
 for product in sponsored_products:
-    try:
-        product_title = product.find_element(By.CLASS_NAME, "pla-unit-title-link").text
-    except NoSuchElementException:
-        product_title = "product_title is empty"
-    print("Title:", product_title)
+    if not('view-all-unit' in product.get_attribute("class")):
+        try:
+            product_title = product.find_element(By.CLASS_NAME, "pla-unit-title-link").text
+        except NoSuchElementException:
+            product_title = "product_title is empty"
+        print("Title:", product_title)
 
-    try:
-        product_url = product.find_element(By.CLASS_NAME, "pla-unit-title-link").get_attribute("href")
-    except NoSuchElementException:
-        product_url = "product_url is empty"
-    print("URL:", product_url)
+        try:
+            product_url = product.find_element(By.CLASS_NAME, "pla-unit-title-link").get_attribute("href")
+        except NoSuchElementException:
+            product_url = "product_url is empty"
+        print("URL:", product_url)
 
-    try:
-        product_price = product.find_element(By.CLASS_NAME, "e10twf").text
-    except NoSuchElementException:
-        product_price = "product_price is empty"
-    print("Price:", product_price)
+        try:
+            product_price = product.find_element(By.CLASS_NAME, "e10twf").text
+        except NoSuchElementException:
+            product_price = "product_price is empty"
+        print("Price:", product_price)
 
-    try:
-        product_rating_star = product.find_element(By.XPATH, "//span[@role = 'img']").get_attribute("aria-label").replace(',', '')
-    except NoSuchElementException:
-        product_rating_star = "product_rating_star is empty"
-    print("Rating Star:", product_rating_star)
+        try:
+            product_rating_star = product.find_element(By.CLASS_NAME, "z3HNkc").get_attribute("aria-label").replace(',', '')
+        except NoSuchElementException:
+            product_rating_star = "product_rating_star is empty"
+        print("Rating Star:", product_rating_star)
 
-    try:
-        product_rating_num_count = product.find_element(By.XPATH, "//span[@class = 'fl pbAs0b']").text
-    except NoSuchElementException:
-        product_rating_num_count = "product_rating_num_count is empty" 
-    print("Rating Num Count:", re.sub('[()]', '', product_rating_num_count))
+        try:
+            product_rating_num_count = product.find_element(By.CLASS_NAME, "pbAs0b").text
+        except NoSuchElementException:
+            product_rating_num_count = "product_rating_num_count is empty" 
+        print("Rating Num Count:", re.sub('[()]', '', product_rating_num_count))
 
-    print("")
+        print("")
+
+print("*** End of sponsored product list ***")
 
 # Close the browser
 driver.quit()
